0fc4a43805b877b3f6a6a66842744c5b4a6ac78e
Leon <leon@Leons-MacBook-Pro.local>
2019-01-30 13:53:34 -0500

    update

---
+++ src/test/java/rocks/zipcode/io/stringparser/ParseCharactersTest.java
@@ -0,0 +1,35 @@
+package rocks.zipcode.io.stringparser;
+
+import org.junit.Test;
+import rocks.zipcode.io.StringParser;
+import rocks.zipcode.io.TestUtils;
+
+/**
+ * @author leon on 10/01/2019.
+ */
+public class ParseCharactersTest {
+    @Test
+    public void test1() {
+        test("123", new Character[]{'1','2','3'});
+    }
+
+    @Test
+    public void test2() {
+        test(" 23", new Character[]{' ', '2','3'});
+    }
+
+    @Test
+    public void test3() {
+        test("   ", new Character[]{' ', ' ', ' '});
+    }
+
+    @Test
+    public void test4() {
+        test(null, null);
+    }
+
+    private void test(String input, Character[] expected) {
+        Character[] actual = StringParser.parseCharacters(input);
+        TestUtils.assertArrayEquals(expected, actual);
+    }
+}
---
+++ src/test/java/rocks/zipcode/io/stringparser/ParseIntegersTest.java
@@ -0,0 +1,35 @@
+package rocks.zipcode.io.stringparser;
+
+import org.junit.Test;
+import rocks.zipcode.io.StringParser;
+import rocks.zipcode.io.TestUtils;
+
+/**
+ * @author leon on 10/01/2019.
+ */
+public class ParseIntegersTest {
+    @Test
+    public void test1() {
+        test("123", new Integer[]{1,2,3});
+    }
+
+    @Test
+    public void test2() {
+        test(" 23", new Integer[]{2,3});
+    }
+
+    @Test
+    public void test3() {
+        test("   ", new Integer[]{});
+    }
+
+    @Test
+    public void test4() {
+        test(null, null);
+    }
+
+    private void test(String input, Integer[] expected) {
+        Integer[] actual = StringParser.parseIntegers(input);
+        TestUtils.assertArrayEquals(expected, actual);
+    }
+}
